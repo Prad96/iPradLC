2ea9bc2f2b228009ad4d7c593f33b3ccda20b38d
iPrad <43658077+Prad96@users.noreply.github.com>
2024-02-24 19:31:28 -0400

    Time: 116 ms (43.78%), Space: 19.3 MB (50.67%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        single_dict={}
         for i in nums:
-            if nums.count(i)==1:
-                return i
-            
+            if i not in single_dict:
+                single_dict[i]=1
+            else:
+                single_dict[i]+=1
+        for key,value in single_dict.items():
+            if value == 1:
+                return key
+        
