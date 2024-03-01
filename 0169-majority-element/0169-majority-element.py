46f3640158fe50326c3b923dd2afee9b60f2ef25
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-01 02:10:09 -0400

    Time: 168 ms (17.16%), Space: 18.1 MB (46.70%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,16 @@
-from collections import Counter
+# from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return max(Counter(nums),key=Counter(nums).get)
+        # return max(Counter(nums),key=Counter(nums).get)
+    
+        nums_dict=defaultdict(int)
+        n = len(nums) // 2
+        for i in nums:
+                nums_dict[i]+=1
+                
+        for key,value in nums_dict.items():
+            if value>n:
+                return key
+                    
+            
+            
