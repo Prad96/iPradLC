b7a2d5c38c3ab9360f678d9705564af0ec173b25
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-04 11:55:01 -0400

    Time: 31 ms (89.68%), Space: 16.5 MB (88.52%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,5 +1,4 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         string=''.join([str(i) for i in digits])
-        string=int(string)+1
-        return [int(i) for i in str(string)]
+        return [int(i) for i in str(int(string)+1)]
