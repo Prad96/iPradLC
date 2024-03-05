13eb97f5804c461875639ab6cd5b1da95d85aa4b
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-05 15:45:17 -0400

    Time: 55 ms (11.79%), Space: 16.7 MB (31.75%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        final_string=''
+        for i in s:
+            if i != 'i':
+               final_string+=i
+            if i=='i':
+               final_string=final_string[::-1]
+        return final_string
+          
