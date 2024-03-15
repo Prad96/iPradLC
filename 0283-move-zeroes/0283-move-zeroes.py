db2d912b7a5a25ad1beb0f1d7fd3dc2903d64f45
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-15 16:34:55 -0300

    Time: 128 ms (54.39%), Space: 18.1 MB (80.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,18 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         
         """
-        # left=0
-        # right=len(nums)-1
-        # while left<right:
-        #       if nums[right] == 0:
-        #         nums[left], nums[right] = nums[right], nums[left]
-        #         right -= 1
-        #       else:
-        #         left += 1
+        left=0
+        right=0
+        while right<len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                right+= 1
+                left+=1
+            else:
+                right += 1
         
         
         
-        zero_count=nums.count(0)
-        for i in range(zero_count):
-            nums.remove(0)
-        nums+=[0]*zero_count
+        # zero_count=nums.count(0)
+        # for i in range(zero_count):
+        #     nums.remove(0)
+        # nums+=[0]*zero_count
