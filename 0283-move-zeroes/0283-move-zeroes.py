e2f4688fef1e40fce73929b09621080d10e0a81a
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-17 15:49:17 -0300

    Time: 141 ms (22.39%), Space: 18 MB (80.00%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,13 +6,13 @@ class Solution:
         """
         left=0
         right=0
+
         while right<len(nums):
             if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
-                right+= 1
-                left+=1
-            else:
-                right += 1
+                left+=1 
+            
+            right += 1
         
         
         
