a094e9bd0325e01ef855aadbfc193f15d7b34675
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-21 23:17:47 -0300

    Time: 35 ms (89.10%), Space: 16.7 MB (30.33%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -3,5 +3,4 @@ class Solution:
         array_dict=dict()
         for element in arr:
             array_dict[element]=array_dict.get(element, 0)+1
-        values=list(array_dict.values())
-        return len(values)==len(set(values))
+        return len(array_dict.values())==len(set(array_dict.values()))
