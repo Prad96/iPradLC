8109c42f74a4b7f6f13ae251eb94914e9052f740
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-25 17:14:28 -0300

    Time: 100 ms (50.23%), Space: 17.8 MB (95.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,7 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         majority=set()
+        nums_dict=defaultdict(int)
         for num in nums:
-            if nums.count(num)>(len(nums)//3):
-                majority.add(num)
-        return majority
+            nums_dict[num]=nums_dict.get(num,0)+1
+        for key,value in nums_dict.items():
+            if value>(len(nums)//3):
+                majority.add(key)
+        return list(majority)
