2b6f57282944b331c97fce0a16593f6666799e6e
iPrad <43658077+Prad96@users.noreply.github.com>
2024-03-29 20:17:23 -0300

    Time: 37 ms (46.73%), Space: 16.6 MB (15.47%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -1,9 +1,7 @@
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
         capitalized=[]
-        titles=title.split()
-        
-        for t in titles:
+        for t in title.split():
             if len(t)>2:
                 capitalized.append(t.capitalize())
             else:
